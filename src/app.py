b4dd4848373b5b0b339c292d4116067be04d17f0
ianlucasch <ianlucaschaves@gmail.com>
2024-07-13 14:59:55 -0300

    Operações bancárias

---
+++ src/app.py
@@ -0,0 +1,58 @@
+menu = """\n
+=============== MENU ===============
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("\n> Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R${valor:.2f}\n"
+        else:
+            print("\n> Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        valor = float(input("\n> Informe o valor de saque: "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("\n> Operação falhou! Você não tem saldo suficiente.")
+        elif excedeu_limite:
+            print("\n> Operação falhou! O valor do saque excede o limite.")
+        elif excedeu_saques:
+            print("\n> Operação falhou! Número máximo de saques excedido.")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R${valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("\n> Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n=============== EXTRATO ===============")
+        print("Não foram realizadas movimentações.\n" if not extrato else extrato)
+        print(f"Saldo: R${saldo:.2f}")
+        print("=======================================")
+
+    elif opcao == "q":
+        print("\n")
+        break
+
+    else:
+        print("\n> Operação inválida! Por favor, selecione novamente a operação desejada.")
